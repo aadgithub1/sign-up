ba3a257adffd6a4ae7b438b84d0d98a15d7eb92c
Aaron <152732836+aadgithub1@users.noreply.github.com>
2024-10-01 10:07:45 -0400

    Fix phone number validation.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let password = document.querySelector("#password")
 let confirmPassword = document.querySelector("#confirm-password")
 let submitBtn = document.querySelector("#submit-btn")
-let email = document.querySelector("#email")
 
 submitBtn.addEventListener("click", function(){
     if (password.value !== confirmPassword.value){
