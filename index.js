3b70f4c40bd7a2606c67c4fe4823f62464e4cedb
Aaron <152732836+aadgithub1@users.noreply.github.com>
2024-10-01 11:26:48 -0400

    Fix password validation border.

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+let form = document.querySelector("form")
 let password = document.querySelector("#password")
-let confirmPassword = document.querySelector("#confirm-password")
+let passwordConfirm = document.querySelector("#confirm-password")
 let submitBtn = document.querySelector("#submit-btn")
+let hint = document.querySelector("#hint")
 
-submitBtn.addEventListener("click", function(){
-    if (password.value !== confirmPassword.value){
-        alert("Passwords do not match")
+passwordConfirm.addEventListener("input", function(){
+    if (password.value !== passwordConfirm.value){
+        password.style.border = "2px solid red"
+        passwordConfirm.style.border = "2px solid red"
+    } else {
+        password.style.border = "2px solid green"
+        passwordConfirm.style.border = "2px solid green"
+    }
+})
+
+password.addEventListener("input", function(){
+    if (password.value !== passwordConfirm.value){
+        password.style.border = "2px solid red"
+        passwordConfirm.style.border = "2px solid red"
+    } else {
+        password.style.border = "2px solid green"
+        passwordConfirm.style.border = "2px solid green"
+    }
+})
+
+form.addEventListener("submit", (e) => {
+    if (password.value !== passwordConfirm.value){
+        e.preventDefault()
+        hint.textContent = "*Passwords do not match"
+        hint.style.color = "red"
+        password.style.border = "2px solid red"
+        passwordConfirm.style.border = "2px solid red"
     }
 })
